ea598d27b835b3d04763e12c105c23a2f09e4139
wendale1231 <spooderm5@gmail.com>
2020-11-28 12:54:12 +0800

    fixed bugs

--- controller/get-sales-data-controller.php
+++ controller/get-sales-data-controller.php
@@ -24,14 +24,15 @@
             echo json_encode($all_data);
         }else if($type == "sales-monthly"){
             $date = new DateTime("now", new DateTimeZone('Asia/Singapore') );
-            $date_now = $date->format("m");
+            $month_now = $date->format("m");
+            $year_now = $date->format("Y");
             $sql = "SELECT * FROM transactions 
                 INNER JOIN 
                     purchased_item ON transactions.transaction_id  =purchased_item.transaction_id 
                 INNER JOIN
                     items ON purchased_item.item_id = items.item_id
                 WHERE 
-                    transactions.transaction_datetime LIKE '$date_now-%-2020'
+                    transactions.transaction_datetime LIKE '$month_now-%-$year_now'
                 AND 
                     transactions.transaction_type = 'incoming'
                 ";
