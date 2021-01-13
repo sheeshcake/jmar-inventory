814c60bdadba419a10e0384a36063d32683cd2ca
wendale1231 <spooderm5@gmail.com>
2021-01-13 15:51:29 +0800

    updated cetegory manager

--- js/category.js
+++ js/category.js
@@ -15,3 +15,28 @@ $(document).on("click", ".cat-del", function() {
         }
     }), $t.row($(this).parents("tr")).remove().draw()
 });
+var edited;
+$(document).on("click", ".cat-up", function() {
+    var t = $t.row($(this).parents("tr")).data();
+    $.ajax({
+        url: url(window.location.href) + "/controller/update-category.php",
+        method: "POST",
+        data: { submit: "submit", id: t[0], name: t[1] },
+        success: function(t) {
+            var e = JSON.parse(t);
+            $("#cat-alert").html('<div class="m-alert alert alert-' + e.status + ' role="alert" style="display:none">' + e.message + "</div>"), $(".m-alert").show(500);
+            $("#category option[value='" + e.id + "']").each(function() {
+                console.log(edited);
+                $(this).html(edited);
+            });
+            $("#sc_" + e.id).html(edited);
+        }
+    })
+});
+$('.cat_edit').blur(function() {
+    var t = $t.row($(this).parents("tr")).data();
+    if (t[1] != $(this).html()) {
+        t[1] = $(this).html();
+        edited = $(this).html();
+    }
+});
