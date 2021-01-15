de17fd9eee141697a05a47596e968b4cfb003784
wendale1231 <spooderm5@gmail.com>
2021-01-15 16:27:43 +0800

    changes to sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo "?p=" . $GLOBALS['roles'][$_SESSION["user"]["role"]]["default"]  ?>">
                 <div class="sidebar-brand-icon">
                     <img src="img/icon-logo.png" alt="" height="50">
                 </div>
