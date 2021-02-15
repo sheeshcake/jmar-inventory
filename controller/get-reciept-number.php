d11e336ddde02f6a2a252b8c4fc600f17db15492
wendale <spooderm5@gmail.com>
2021-02-15 14:12:14 +0800

    [FIX] get reciept query

--- controller/get-reciept-number.php
+++ controller/get-reciept-number.php
@@ -1,6 +1,6 @@
 <?php
     include "../controller/connect.php";
-    $sql = "SELECT * FROM transactions ORDER BY reciept_no DESC limit 1";
+    $sql = "SELECT * FROM transactions WHERE transaction_id = (SELECT MAX(transaction_id) FROM transactions)";
     $result = mysqli_query($conn, $sql);
     echo json_encode($result->fetch_assoc());
 ?>
