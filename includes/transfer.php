8dc3f8d4ba71d26f77008cfddfbeac08b17e4bd9
wendale <spooderm5@gmail.com>
2021-02-23 14:06:52 +0800

    bug fix and init col row

---
+++ includes/transfer.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <table></table>
+        </div>
+    </div>
+    <div class="col-auto">
+    <div class="card mx-auto">
+    hello
+    </div>
+    
+    </div>
+    <div class="col">
+        <div class="card">
+            
+        </div>
+    </div>
+
+</div>
